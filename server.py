621aa3b1ef5da9475c6eb316fe04e53eae6b62e9
J Nguyen <3u.ngn@pm.me>
2022-07-09 20:59:19 -0700

    commit

--- server.py
+++ server.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def init_checkerboard():
+def checkerboard():
     return render_template("index.html", rows=8, cols=8, color_1="black", color_2="red")
 
 
 @app.route('/<int:rows>/', methods=['GET'])
-def change_rows(rows):
+def rows(rows):
     return render_template("index.html", rows=rows, cols=8, color_1="black", color_2="red")
 
 
 @app.route('/<int:rows>/<int:columns>/', methods=['GET'])
-def change_rows_colums(rows, columns):
+def columns(rows, columns):
     return render_template("index.html", rows=rows, cols=columns, color_1="black", color_2="red")
 
 
 @app.route('/<int:rows>/<int:columns>/<string:color_1>/', methods=['GET'])
-def change_rows_colums_color(rows, columns, color_1):
+def color(rows, columns, color_1):
     return render_template("index.html", rows=rows, cols=columns, color_1=color_1, color_2="red")
 
 
 @app.route('/<int:rows>/<int:columns>/<string:color_1>/<string:color_2>/', methods=['GET'])
-def change_rows_colums_colors(rows, columns, color_1, color_2):
+def rows_columns_colors(rows, columns, color_1, color_2):
     return render_template("index.html", rows=rows, cols=columns, color_1=color_1, color_2=color_2)
 
 
